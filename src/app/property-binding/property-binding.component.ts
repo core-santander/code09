435fbfd376e915ebc4ed0a0a0873f18a39eedc8a
core-santander <pugipedro@gmail.com>
2018-07-09 17:22:02 +0200

    pr v

---
+++ src/app/property-binding/property-binding.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-binding',
+  templateUrl: './property-binding.component.html',
+  styleUrls: ['./property-binding.component.css']
+})
+export class PropertyBindingComponent implements OnInit {
+
+  imagen = 'assets/santander_aerea.jpg';
+
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() =>{
+      this.imagen = 'assets/santander.jfif';
+    }, 3000);
+  }
+
+}
